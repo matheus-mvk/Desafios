4f301ab7ccfcb8d53590f1b538ebaec9fd38316c
matheus-mvk <matheusmhvs1@gmail.com>
2022-06-05 11:44:02 -0300

    Toastify complete: loading and animation bar.

---
+++ Toasty/script.js
@@ -0,0 +1,29 @@
+const btn = document.querySelector(".btn");
+const divMessage = document.querySelector(".alert");
+
+const msg = "Promise is pending";
+
+function ativar(msg){
+    const message = document.createElement("div");
+    message.classList.add("message");
+    message.innerText = msg;
+    divMessage.appendChild(message);
+
+    // Loading
+    //const load = document.createElement("div");
+    //load.classList.add("loading");
+    
+    //divMessage.appendChild(load)
+
+    
+
+    setTimeout(()=>{
+        message.style.display = "none";
+        //load.style.display = "none";
+    }, 3000)
+}
+
+
+btn.addEventListener("click",()=>{
+    ativar(msg)
+});
